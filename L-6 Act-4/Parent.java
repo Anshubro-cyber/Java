4ed59bf0f031cd7d2aabe025b1dec0f989fde9a5
Anshubro-cyber <akshatagrawal0831@gmail.com>
2025-08-08 16:39:23 +0530

    Acess

---
+++ L-6 Act-4/Parent.java
@@ -0,0 +1,20 @@
+class Parent {
+    protected void protect() {
+        System.out.println("I'm inside protected method");
+    }
+}
+
+class Child extends Parent {
+    private void privateMethod() {
+        System.out.println("I'm inside private method");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Child kid = new Child();
+        // kid.privateMethod();
+
+        System.out.println("Hello world!");
+    }
+}
