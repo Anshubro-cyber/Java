aa05cd824bad6bc00c80fd9100e0710c9b8f90a0
Anshubro-cyber <akshatagrawal0831@gmail.com>
2025-08-26 18:46:29 +0530

    Magic1

--- L-10 Act-2/Main.java
+++ L-10 Act-2/Main.java
@@ -36,7 +36,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to InterHiring Tool");
+        System.out.println("Welcome to InternHiring Tool");
 
         try {
             Candidate candidate = getCandidateDetails();
